4efae697dd2b1b6ba9b6061e48fd432770ccf9ba
mackmcquen <mackmcquen@comcast.net>
2020-04-22 11:11:28 -0500

    Done.

--- app.rb
+++ app.rb
@@ -14,5 +14,14 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get "/goodbye/:name" do
+    @person_name = params[:name]
+    "Goodbye, #{@person_name}."
+  end
 
+  get "/multiply/:num1/:num2" do
+    @num_one = params[:num1].to_i
+    @num_two = params[:num2].to_i
+    "#{@num_one * @num_two}"
+  end
 end
